bba8d902254b096c19a2afcf307b0576267c125a
MethodGirl <94073698+MethodGirl@users.noreply.github.com>
2026-02-11 16:58:44 +0300

    Update hw-6.js

--- js/hw-6.js
+++ js/hw-6.js
@@ -9,7 +9,7 @@ console.log(str)
 
 function filterString(arr, str) {
     let lowerStr = str.toLowerCase()
-    
+
     return arr.filter((el) => {
         return el.toLowerCase().startsWith(lowerStr)
     })
@@ -34,7 +34,7 @@ console.log(Math.min(...numberArr))
 
 //5
 
-function randomNum(){
+function randomNum() {
     return Math.floor(Math.random() * 10) + 1
 }
 
@@ -42,3 +42,77 @@ randomNum()
 
 //6
 
+function randomArr(n) {
+    let result = Math.trunc(n / 2)
+    let arr6 = []
+
+    for (let i = 0; i < result; i++) {
+        arr6.push(Math.floor(Math.random() * n))
+    }
+
+    return arr6
+}
+
+randomArr(7)
+
+//7
+
+function randomBetween(num1, num2) {
+    let max = Math.max(num1, num2)
+    let min = Math.min(num1, num2)
+
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+randomBetween(10, 25)
+
+//8
+
+console.log(new Date())
+
+//9
+
+let currentDate = new Date();
+let dateNow = +currentDate
+
+let days73 = 73 * 24 * 60 * 60 * 1000;
+
+let searchDate = dateNow + days73
+
+let days73Hence = new Date(searchDate)
+
+console.log(days73Hence)
+
+// 10
+
+function getDateInfo() {
+    let currentDate = new Date()
+
+    let data = currentDate.getDate()
+    let month = currentDate.getMonth()
+    let year = currentDate.getFullYear()
+
+    let weekDay = currentDate.getDay()
+
+    let hour = currentDate.getHours()
+    let minutes = currentDate.getMinutes()
+    let seconds = currentDate.getSeconds()
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    hour = hour < 10 ? '0' + hour : hour;
+
+    const months = [
+        'января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+        'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
+    ]
+
+    const weekdays = [
+        'воскресенье', 'понедельник', 'вторник', 'среда',
+        'четверг', 'пятница', 'суббота'
+    ]
+
+    return `Дата: ${data} ${months[month]} ${year} — это ${weekdays[weekDay]} Время: ${hour}:${minutes}:${seconds}`
+}
+
+getDateInfo()
